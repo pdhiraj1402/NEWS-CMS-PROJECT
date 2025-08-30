271419bb0da020db80b3255fb6f8df66e9b22fc3
Dhiraj Prajapati <1997dhirajprajapati@gmail.com>
2025-08-30 15:56:09 +0530

    Controllers Are Created

---
+++ controllers/categoryController.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const categoryModel = require('../models/Category');
+
+const allcategory = async (req, res) => { };
+const addCategoryPage = async (req, res) => { };
+const addCategory = async (req, res) => { };
+const updateCategoryPage = async (req, res) => { };
+const updateCategory = async (req, res) => { };
+const deleteCategory = async (req, res) => { };
+
+module.exports = {
+    allcategory,
+    addCategoryPage,
+    addCategory,
+    updateCategoryPage,
+    updateCategory,
+    deleteCategory
+};
---
+++ routes/admin.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const router = express.Router();
+
+const articleController = require('../controllers/articleController');
+const categoryController = require('../controllers/categoryController');
+const commentController = require('../controllers/commentController');
+const userController = require('../controllers/userController');
+
+//  Login Routes
+router.get('/', userController.loginPage());
+router.get('/index', userController.adminLogin());
+router.get('/logout', userController.logout());
+
+//  User CRUD Routes
+router.get('/users', userController.allUser());
+router.get('/add-user', userController.addUserPage());
+router.post('/add-user', userController.addUser());
+router.get('/update-user', userController.updateUserPage());
+router.post('/update-user', userController.updateUser());
+router.get('/delete-user/:id', userController.deleteUser());
+
+//  Category CRUD Routes
+router.get('/category', categoryController.allcategory());
+router.get('/add-category', categoryController.addCategoryPage());
+router.post('/add-category', categoryController.addCategory());
+router.get('/update-category', categoryController.updateCategoryPage());
+router.post('/update-category', categoryController.updateCategory());
+router.get('/delete-category/:id', categoryController.deleteCategory());
+
+//  Article CRUD Routes
+router.get('/article', articleController.allArticle());
+router.get('/add-article', articleController.addArticlePage());
+router.post('/add-article', articleController.addArticle());
+router.get('/update-article', articleController.updateArticlePage());
+router.post('/update-article', articleController.updateArticle());
+router.get('/delete-article/:id', articleController.deleteArticle());
+
+//  Comment Routes
+router.get('/comments', commentController.allComments());
+
+module.exports = router;
